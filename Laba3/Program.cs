85e51a583798b0af7a36c081062498b7bd407669
ArtemkaGoldMan <artemka.omelchenko@gmail.com>
2022-06-15 15:11:43 +0200

    New v

--- Laba3/Program.cs
+++ Laba3/Program.cs
@@ -1,74 +1,131 @@
 ﻿using System;
 
-namespace Laba3
+namespace LabaProgaAssist
 {
-    class Drib
-    {
-        public double Chyseln { get; set; }
-        public double Znamenn { get; set; }
-        public string znak { get; set; }
-        public static string MakeDrib(Chyslo chyslo, Znak znak)
-        {
-            return znak.znak + (chyslo.Chyseln / chyslo.Znamenn).ToString();
-            
-        }
-        public static double Plus(double Drib1, double Drib2)
-        {
-            return Drib1 + Drib2;
-        }
-        public static double Minus(double Drib1, double Drib2)
-        {
-            return Drib1 - Drib2;
-        }
-        public static double Mnozenia(double Drib1, double Drib2)
-        {
-            return Drib1 * Drib2;
-        }
-        public static double dilenia(double Drib1, double Drib2)
-        {
-            return Drib1 / Drib2;
-        }
-
-    }
-    class Chyslo : Drib
-    {
-        
-        public Chyslo(int Chyselnyk,int Znamennyk)
-        {
-            Chyseln = Chyselnyk;
-            Znamenn = Znamennyk;
-
-        }
-       
-    }
-    class Znak:Drib
-    {
-        
-        public Znak(string zn)
-        {
-            this.znak = zn;
-        }
-    }
-  
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Chyslo chyslo1 = new Chyslo(1,2);
-            Znak znak1 = new Znak("-");
-            double Drib1 = Double.Parse(Drib.MakeDrib(chyslo1,znak1));
-            Console.WriteLine("Drib1= " + Drib1);
+            Drib drib1 = new Drib(1 ,2,'-');
 
-            Chyslo chyslo2 = new Chyslo(5, 7);
-            Znak znak2 = new Znak("-");
-            double Drib2 = Double.Parse(Drib.MakeDrib(chyslo2, znak2));
-            Console.WriteLine("Drib2= " + Drib2);
+            Drib drib2 = new Drib(1, 2, '+');
 
-            Console.WriteLine("Plus = "+ Drib.Plus( Drib1, Drib2));
-            Console.WriteLine("Minus = " + Drib.Minus(Drib1, Drib2));
-            Console.WriteLine("Mnozenia = " + Drib.Mnozenia(Drib1, Drib2));
-            Console.WriteLine("Dilenia = " + Drib.dilenia(Drib1, Drib2));
+            drib1.WriteDrib();
+            drib2.WriteDrib();
+            drib1.Multiplication(drib2);
+            drib1.Division(drib2 );
+            drib1.Minus(drib2);
+            drib1.Plus(drib2);
+
         }
-       
+        
+    }
+
+    class Number
+    {
+        public double Value { get; set; }
+        public Number(double numb)
+        {
+            this.Value = numb;
+        }
+    }
+
+    class Znak
+    {
+        public char Value { get; set; }
+        public Znak(char numb)
+        {
+            this.Value = numb;
+        }
+    }
+
+    class Drib
+    {
+
+        public Znak Znak;
+        public Number Chiselnyk;
+        public Number Znamenyk;
+        
+        public Drib(double chyselnyk, double znamennyk, char znak )
+        {
+            this.Chiselnyk = new Number (chyselnyk);
+            this.Znamenyk = new Number(znamennyk);
+            this.Znak = new Znak(znak);
+        }
+
+        public void WriteDrib()
+        {
+            Console.WriteLine(Znak.Value.ToString()+""+Chiselnyk.Value+"/"+Znamenyk.Value);
+        }
+        public void Multiplication(Drib drib2)
+        {
+            if(Znak.Value != drib2.Znak.Value)
+            {
+                Console.WriteLine("-"+(Chiselnyk.Value*drib2.Chiselnyk.Value)+"/"+(Znamenyk.Value*drib2.Znamenyk.Value)+" = "+ (-1*(Chiselnyk.Value * drib2.Chiselnyk.Value)/(Znamenyk.Value * drib2.Znamenyk.Value)));
+            }
+            else
+            {
+                Console.WriteLine("Multiplication " + (Chiselnyk.Value * drib2.Chiselnyk.Value) + "/" + (Znamenyk.Value * drib2.Znamenyk.Value) + " = " +(Chiselnyk.Value * drib2.Chiselnyk.Value)/(Znamenyk.Value * drib2.Znamenyk.Value));
+            }
+        }
+
+        public void Division(Drib drib2)
+        {
+            if (Znak.Value != drib2.Znak.Value)
+            {
+                Console.WriteLine("-" + (Chiselnyk.Value * drib2.Znamenyk.Value) + "/" + (Znamenyk.Value * drib2.Chiselnyk.Value) + " = " + (-1 * (Chiselnyk.Value * drib2.Znamenyk.Value) / (Znamenyk.Value * drib2.Chiselnyk.Value)));
+            }
+            else
+            {
+                Console.WriteLine("Division "+ (Chiselnyk.Value * drib2.Znamenyk.Value) + "/" + (Znamenyk.Value * drib2.Chiselnyk.Value) + " = " + (Chiselnyk.Value * drib2.Znamenyk.Value) / (Znamenyk.Value * drib2.Chiselnyk.Value));
+            }
+        }
+
+        public void Plus(Drib drib2)
+        {
+            int znak1;
+            
+            if(Znak.Value == '-')
+            {
+                znak1 = -1;
+            }
+            else
+            {
+                znak1 = 1;
+            }
+            int znak2;
+            if (drib2.Znak.Value == '-')
+            {
+                znak2 = -1;
+            }
+            else
+            {
+                znak2 = 1;
+            }
+            Console.WriteLine("Plus " + ((znak1* Chiselnyk.Value* drib2.Znamenyk.Value) +(znak2* drib2.Chiselnyk.Value*Znamenyk.Value))/(Znamenyk.Value*drib2.Znamenyk.Value));
+        }
+
+        public void Minus(Drib drib2)
+        {
+            int znak1;
+            int znak2;
+            if (Znak.Value == '-')
+            {
+                znak1 = -1;
+            }
+            else
+            {
+                znak1 = 1;
+            }
+            if (drib2.Znak.Value == '-')
+            {
+                znak2 = -1;
+            }
+            else
+            {
+                znak2 = 1;
+            }
+            Console.WriteLine("Minus "+ ((znak1 * Chiselnyk.Value * drib2.Znamenyk.Value) - (znak2 * drib2.Chiselnyk.Value * Znamenyk.Value)) / (Znamenyk.Value * drib2.Znamenyk.Value));
+        }
     }
 }
